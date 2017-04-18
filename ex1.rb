a6c9b7b24bcf6bee51bb1c1c77988be6151cabf1
ParthaB <pbp2005@gmail.com>
2017-04-17 21:18:28 -0400

    2 & 3

---
+++ ex1.rb
@@ -0,0 +1,6 @@
+# Ruby exercise 1.
+# comments and printing
+puts "Hello world"
+puts "This is how lines are printed in ruby."
+puts "Printing..."
+# end of file
---
+++ ex2.rb
@@ -0,0 +1,15 @@
+# Ruby exercise 2
+# playing with numbers and math
+
+puts "Chicken count:"
+puts "Hens", 25 + (30/6)
+puts "Roosters", 100 - 25 * (3%4)
+puts "Egg count:"
+puts "Is it true that 3 + 5 < 5 - 7?"
+puts 3 + 5 < 5 - 7
+puts "what is 3 + 2?", 3 + 2
+puts "What is 5 - 7?", 5 - 7
+puts "Is it greater? 5 > -2", 5 > -2
+puts "Is it less or equal? 5 <= -2", 5 <= -2
+
+# end of file
