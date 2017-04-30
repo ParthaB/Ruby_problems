7d4d0a3d12e1d8011e2a6c759d8c7c67e972a8d9
ParthaB <pbp2005@gmail.com>
2017-04-30 13:30:34 -0400

    commit

---
+++ palindrome.rb
@@ -0,0 +1,19 @@
+def palindrome?(string)
+	t1 = Time.now
+	arr = []
+	i = 0
+	while i < string.length
+		arr.unshift(string[i])
+		i += 1
+	end
+	string2 = arr.join
+	if string2 == string
+		true 
+	else 
+		false
+	end
+	t2 = Time.now
+	puts delta = (t2-t1) *1000
+end
+
+puts palindrome?("abcba")
