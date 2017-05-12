8d0a5ef8783126773906608c6fe804fc498b8a23
ParthaB <pbp2005@gmail.com>
2017-05-11 20:28:34 -0400

    projecteuler3.net

--- prob3.rb
+++ prob3.rb
@@ -3,5 +3,28 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 
 600851475143 ?
-=end
 
+
+def prime_factor(n)
+	prime_arr = []
+	p = 2
+	if n < 2
+		return p
+	end
+
+	while p < n 
+		if n % p == 0
+			prime_arr.push(p)
+		end
+		p += 1
+	end
+
+	return prime_arr.max
+end
+
+puts prime_factor(30)
+=end
+require 'prime'
+n = 600851475143
+largest = n.prime_division.max #_by(&:first).first
+puts largest.max
