406db4cf900ce3dedc32210ec8df73abae632bcb
ParthaB <pbp2005@gmail.com>
2017-05-17 23:09:33 -0400

    euler3

--- prob3.rb
+++ prob3.rb
@@ -1,4 +1,3 @@
-=begin
 The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 
@@ -23,8 +22,10 @@ def prime_factor(n)
 end
 
 puts prime_factor(30)
-=end
+
+=begin
 require 'prime'
 n = 600851475143
 largest = n.prime_division.max #_by(&:first).first
 puts largest.max
+=end
